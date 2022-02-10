6314d4c78682cd794983f4edf126789f7093d908
Enery19 <imencru634@g.educaand.es>
2022-02-10 22:26:48 +0100

    10/02

---
+++ Práctica 9 - Validación formularios/validacion.js
@@ -0,0 +1,7 @@
+var name = document.getElementById('ipt_name').value;
+var surname = document.getElementById('ipt_surname').value;
+var email = document.getElementById('ipt_email').value;
+var texto = document.getElementById('txt_area').value;
+
+var publi = document.getElementById('chk_publi');
+var paises = document.getElementById('slct_procedencia');
